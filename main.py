543ad15606c4b324e3ad34c0025ba36aec1f1af7
code-skg <sattuamar123@gmail.com>
2026-03-03 23:31:42 +0530

    Refactor logger.py: Comment out main execution block for cleaner module usage

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import sys
+from src.logger import logging
 from src.exception import CustomException
 
 try:
     a = 1 / 0
 except Exception as e:
+    logging.info("devide by zero error")
     raise CustomException(e, sys)
--- src/logger.py
+++ src/logger.py
@@ -15,5 +15,5 @@ logging.basicConfig(
     level=logging.INFO,
 )
 
-if  __name__ ==  "__main__":
-    logging.info("loggin just started !")
+# if  __name__ ==  "__main__":
+#     logging.info("loggin just started !")
